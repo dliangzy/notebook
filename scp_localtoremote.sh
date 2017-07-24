58fdf6c74422c0a8eb9446cc24d72c612a2b6a00
liang <liuliangshine@gmail.com>
2017-07-24 17:11:31 +0800

    scp script

---
+++ scp_localtoremote.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#This script is used to copy file from localhost to remote host
+passwd="LL@ustc0629\r"
+file=$1
+expect<<-END
+spawn scp $file lliu@ui06.lcg.ustc.edu.cn:/home/lliu/scp/
+expect "password: "
+send $passwd
+expect eof
+exit
+END
